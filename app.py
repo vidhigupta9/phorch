00483fc1fd339badac3c7fe176116e4bb23bc10a
vidhigupta9 <gvidhi9@gmail.com>
2021-11-02 05:03:04 +0530

    adding app.py

---
+++ app.py
@@ -0,0 +1,142 @@
+import streamlit as st
+import warnings
+warnings.filterwarnings("ignore")
+# EDA Pkgs
+import pandas as pd
+import numpy as np
+import pandas as pd
+import tweepy
+import json
+from tweepy import OAuthHandler
+import re
+import textblob
+from textblob import TextBlob
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import openpyxl
+import time
+import tqdm
+
+#To Hide Warnings
+st.set_option('deprecation.showfileUploaderEncoding', False)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Viz Pkgs
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+import seaborn as sns
+#sns.set_style('darkgrid')
+
+
+STYLE = """
+<style>
+img {
+    max-width: 100%;
+}
+</style> """
+
+def main():
+    """ Common ML Dataset Explorer """
+    #st.title("Phorch")
+    #st.subheader("Don't be the victim!")
+
+    html_temp = """
+	<div style="background-color:tomato;"><p style="color:white;font-size:40px;padding:9px">Phorch</p></div>
+	"""
+    st.markdown(html_temp, unsafe_allow_html=True)
+    st.subheader("Dont'be the victim!")
+
+    ################# Twitter API Connection #######################
+    consumer_key = "yJJ9L9NpkbzbXoCtq3GvWnp9m"
+    consumer_secret = "fvwJZKBYyEgFSEuWlZ60QKPouiYxDiLPzDwFBIeIWX9xp6Xjg8"
+    access_token = "3104698248-SYpGE0rsdf7xIbF6ZES8IxQ561d9zPnRWqfpEjS"
+    access_token_secret = "7jl7Qyuyy0oZxSHR99H26PdJg96owgCZGnlUWUdVsbWif"
+
+
+
+    # Use the above credentials to authenticate the API.
+
+    auth = tweepy.OAuthHandler( consumer_key , consumer_secret )
+    auth.set_access_token( access_token , access_token_secret )
+    api = tweepy.API(auth)
+    ################################################################
+    
+    df = pd.DataFrame(columns=["Date","User","IsVerified","Tweet","Likes","RT",'User_location'])
+    
+    # Write a Function to extract tweets:
+    def get_tweets(Topic,Count):
+        i=0
+        #my_bar = st.progress(100) # To track progress of Extracted tweets
+        for tweet in tweepy.Cursor(api.search_tweets, q=Topic,count=50, lang="en",exclude='retweets').items():
+            #time.sleep(0.1)
+            #my_bar.progress(i)
+            df.loc[i,"Date"] = tweet.created_at
+            df.loc[i,"User"] = tweet.user.name
+            df.loc[i,"IsVerified"] = tweet.user.verified
+            df.loc[i,"Tweet"] = tweet.text
+            df.loc[i,"Likes"] = tweet.favorite_count
+            df.loc[i,"RT"] = tweet.retweet_count
+            df.loc[i,"User_location"] = tweet.user.location
+            #df.to_csv("TweetDataset.csv",index=False)
+            #df.to_excel('{}.xlsx'.format("TweetDataset"),index=False)   ## Save as Excel
+            i=i+1
+            if i>Count:
+                break
+            else:
+                pass
+    # Function to Clean the Tweet.
+    
+    def clean_tweet(tweet):
+        regex=r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
+        matches = re.findall(regex, tweet)
+        return matches
+    
+
+    
+    #
+    #from PIL import Image
+    #image = Image.open('Logo1.jpg')
+    #st.image(image, caption='Twitter for Analytics',use_column_width=True)
+    
+    
+    # Collect Input from user :
+    Topic = str()
+    Topic = str(st.text_input("Enter the topic you are interested in (Press Enter once done)"))     
+    
+    if len(Topic) > 0 :
+        
+        # Call the function to extract the data. pass the topic and filename you want the data to be stored in.
+        with st.spinner("Please wait, Tweets are being extracted"):
+            get_tweets(Topic , Count=10)
+        st.success('Tweets have been Extracted !!!!')    
+           
+
+        # Call function to get Clean tweets
+        df['clean_tweet'] = df['Tweet'].apply(lambda x : clean_tweet(x))
+    
+
+        # See the Extracted Data : 
+        if st.button("See the Extracted Data"):
+            #st.markdown(html_temp, unsafe_allow_html=True)
+            st.success("Below is the Extracted Data :")
+            st.write(df.clean_tweet)        
+        
+        
+    
+    st.sidebar.header("About App")
+    st.sidebar.info("A Twitter Sentiment analysis Project which will scrap twitter for the topic selected by the user. The extracted tweets will then be used to determine the Sentiments of those tweets. \
+                    The different Visualizations will help us get a feel of the overall mood of the people on Twitter regarding the topic we select.")
+    #st.sidebar.text("Built with Streamlit")
+    
+    st.sidebar.header("Made by :")
+    st.sidebar.info("Vidhi Gupta")
+    st.sidebar.info("Deep Rodge")
+    
+
+if st.button("Exit"):
+        st.balloons()
+
+
+
+if __name__ == '__main__':
+    main()
