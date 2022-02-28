fb2fd8ff04121afcbff71228bdc335d74fa76ce2
vidhigupta9 <gvidhi9@gmail.com>
2022-03-01 04:57:05 +0530

    Application

--- README.md
+++ README.md
@@ -1,20 +1,26 @@
-# Phorch
+# Phizon
 
-Malicious web site is a foundation of criminal activities on Internet. This links enables partial 
-or full machine control to the attackers. This results in victim systems, which get
-easily infected allowing attackers to utilize systems for quite a number of cyber-crimes such
-as stealing credentials, spamming, phishing, denial-of-service and many extra such attacks. 
+The base of any illicit activity on the Internet is a malicious web site. The attackers can gain partial or complete control of the machine using these linkages. As a result, target systems become readily infected, allowing attackers to use them for a variety of cyber-crimes such as credential theft, spamming, phishing, denial-of-service assaults, and other similar attacks.
 The rising issue related to spamming, phishing and malware, has created a requirement for solid
-framework solution which can analyze the extracted features, classify and further recognize the malicious URL.
+framework solution which can analyze the extracted features, classify and further recognize the malicious URL as well as images of clone websites.
 Therefore, the methodology and technique to detect such crimes should be fast and precise
 with the additional capability to detect new malicious websites or content. 
 <br />
 <br />
-This repository introduces an automatic tool to extract 111 significant features for a URL. Additionally, this
-also proposes the URL classification process which recognizes whether the target website is a malicious(1) or benign(0).
 
-[URL.md](https://github.com/vidhigupta9/pytorch/blob/main/URL.md) provides detailed information and methods on how to extract features from urls for prediction and also how to create your own database.
+## What we do
+### URL Check
+This repository introduces an automatic tool to extract 111 significant features for a URL. Additionally, this
+also proposes the URL classification process which recognizes whether the target website is a malicious(1) or benign(0). User can enter any URL of choice and even randomly use it on tweets in real time to observe any malicious activity.
+
+[Url.md](https://github.com/vidhigupta9/aws/blob/main/Url.md) provides detailed information and methods on how to extract features from urls for prediction and also how to create your own database.
 <br />
+
+### Clone Check
+Upload a website screenshot/image to see whether it is a clone of any authorised popular website to prevent sensitive information retrieval and data breaches. Classify it using our deep learning model trained on Habana DL1 accelerators.
+<br />
+
+## Dataset
 
 ## Install
 
--- app.py
+++ app.py
@@ -26,10 +26,10 @@ img {
 def main():
 
     html_temp = """
-    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
-	    <div style="background-color:rgb(143, 38, 255);">
-            <p style="color:white;font-size:50px;padding:9px;text-transform:uppercase;font-family:Open Sans">
-                <b>Phorch</b>
+    <link href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap' rel='stylesheet'>
+	    <div style="background-color:#8C1515;">
+            <p style="color:white;font-size:50px;padding:9px;text-transform:uppercase;font-family:Open Sans,sans-serif;;letter-spacing: 3px">
+                <b>Phizon</b>
             </p>
         </div>
 	"""
@@ -37,15 +37,15 @@ def main():
     st.subheader("Don't be the victim!")
  
     from PIL import Image
-    image = Image.open('phish.gif')
+    image = Image.open('phish.png')
     st.image(image,use_column_width=True)
 
     ################# Twitter API Connection #######################
     
-    consumer_key = "fktGsm2e6VfvCUZNflgwCnyOk"
-    consumer_secret = "vD2cBuXWN6fm4mdpDJepdDfoVGI0JEqmTlJT1tXAmISWJm8rrr"
-    access_token = "3104698248-t27PxxE2sAsV81NH45hiVhynNrB1rZaLau6eJdl"
-    access_token_secret = "8LdCL8LUbtCzszNmmzhndZpN7k1YkXbyupKLfvIy10YHy"
+    consumer_key = "kvSRXawYtSh32pknAZh7NRAFv"
+    consumer_secret = "yEWEEnR5wqWUxUUUDT03N3uIpfDbdN5kgNOqXXQinWrqo2os2o"
+    access_token = "3104698248-9cv98abLhM1GTSQFZNGTdpYDgjqpra8JY7XxWca"
+    access_token_secret = "6na5TRSSNIck1smYecrygwSZBosbCW6yxkFxmHSyIvrEw"
 
     # Use the above credentials to authenticate the API.
 
@@ -59,13 +59,24 @@ def main():
         dt = detector.Detector()
         result = dt.detect(url)
         return result 
+    
+    def image_check():
+        st.subheader("Image")
+        image_file = st.file_uploader("Upload Image", type=["png","jpg","jpeg"])
+        if image_file is not None:
+            file_details = {"filename":image_file.name, "filetype":image_file.type}
+            st.write(file_details)
+            st.image(image_file)
+            my_bar = st.progress(100)
+
 
     def check_url():
         # Collect Input from user :
         url = str()
-        url = str(st.text_input("Enter the URL you wish to indentify (Press Enter once done)"))
+        url = str(st.text_input("Enter the URL you wish to identify (Press Enter once done)"))
         result = prediction(url)
         return result
+
     
     def get_tweets(Topic,Count):
                 i=0
@@ -87,7 +98,7 @@ def main():
                     else:
                         pass
                 return df
-            # Function to Clean the Tweet
+
     def clean_tweet(tweet):
                 regex=r"\b((?:https?://)?(?:(?:www\.)?(?:[\da-z\.-]+)\.(?:[a-z]{2,6})|(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)|(?:(?:[0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,7}:|(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|(?:[0-9a-fA-F]{1,4}:){1,5}(?::[0-9a-fA-F]{1,4}){1,2}|(?:[0-9a-fA-F]{1,4}:){1,4}(?::[0-9a-fA-F]{1,4}){1,3}|(?:[0-9a-fA-F]{1,4}:){1,3}(?::[0-9a-fA-F]{1,4}){1,4}|(?:[0-9a-fA-F]{1,4}:){1,2}(?::[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:(?:(?::[0-9a-fA-F]{1,4}){1,6})|:(?:(?::[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(?::[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(?:ffff(?::0{1,4}){0,1}:){0,1}(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])|(?:[0-9a-fA-F]{1,4}:){1,4}:(?:(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(?:25[0-5]|(?:2[0-4]|1{0,1}[0-9]){0,1}[0-9])))(?::[0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?(?:/[\w\.-]*)*/?)\b"
                 matches = re.findall(regex, tweet)
@@ -103,7 +114,7 @@ def main():
             # Write a Function to extract tweets:
             # Collect Input from user :
             Topic = str()
-            Topic = str(st.text_input("Enter the topic you are interested in (Press Enter once done)"))
+            Topic = str(st.text_input("(Press Enter once done)"))
             if len(Topic) > 0 :
                 # Call the function to extract the data. pass the topic and filename you want the data to be stored in.
                 with st.spinner("Please wait, Tweets are being extracted"):
@@ -125,7 +136,7 @@ def main():
 
     process_name = st.selectbox(
     'Select Operation',
-    ('Select','Enter URL','Fetch Tweets')
+    ('Select','Enter URL','Fetch Tweets','Image Check')
     )
 
     if process_name == 'Enter URL':
@@ -133,16 +144,19 @@ def main():
         if st.button("Predict Results"):
             st.write(result)
     if process_name == 'Fetch Tweets':
+        st.write("Enter any topic to fetch random tweets and verify if they promote any malware activity through hyperlinks")
         fetch_tweet(api)               
+    if process_name == 'Image Check':
+        st.write("Upload any image of potential phishing website to verify if it is clone or not")
+        image_check()
+
    
     
     #Sidebar
     st.sidebar.header("About App")
-    st.sidebar.info("A Twitter Sentiment analysis Project which will scrap twitter for the topic selected by the user. The extracted tweets will then be used to determine the Sentiments of those tweets. \
-                    The different Visualizations will help us get a feel of the overall mood of the people on Twitter regarding the topic we select.")
-    #st.sidebar.text("Built with Streamlit")
+    st.sidebar.info("Lookout if you have been a victim of phishing attack or check before you become one.")
     
-    st.sidebar.info("[Source Code](https://github.com/vidhigupta9/pytorch)")
+    st.sidebar.info("[Source Code](https://github.com/vidhigupta9/aws)")
     st.sidebar.header("Made by :")
     st.sidebar.info("[Vidhi Gupta](https://github.com/vidhigupta9)")
     st.sidebar.info("[Deep Rodge](https://github.com/deeprodge)")
@@ -150,7 +164,6 @@ def main():
 
 if st.button("Exit"):
         st.balloons()
-
 
 
 if __name__ == '__main__':
